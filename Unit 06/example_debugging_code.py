8ea7dfff3052ab1bb1f35e01e00756f38b3779e7
Tianyu W <tianyuelite@gmail.com>
2022-09-07 10:02:08 -0400

    add unit 06

---
+++ Unit 06/example_debugging_code.py
@@ -0,0 +1,49 @@
+# Fix the code, syntax
+
+greeting = input("Hello, possible pirate! What's the password?)
+if greeting = "Arrr!'
+    print("Go away, pirate.")
+elif
+print("Greetings, pirates!")
+
+
+# Fix the following code, SyntaxError
+year == int.input("Greetings! What is your year of origin? ')
+if year <= 1900
+    print('Woah, that's the past!")
+elif year > 1900 && year < 2021:
+    	print("That's totally the present!")
+	elif:
+    	print("Far out, that's the future!!")
+
+
+# Fix the following code, NameError
+def problem4():
+    total = 0
+    count = 0
+    for i in range(100, 201):
+        if is_prime(i):
+            total += i
+            count += 1
+    average = total/count
+    return(total, average)
+
+def is_Prime(number):
+    if(number < 2):
+        return False
+    for x in range(2, number):
+        if number % x == 0:
+            return False
+    return True
+print(problem4())
+
+
+# Fix the following code, TypeError
+def problem1(s):
+    total = 0
+    for i in s:
+        total += i
+    return total
+
+print(problem1('12345'))
+
---
+++ Unit 06/exercise_catch_zero_division.py
@@ -0,0 +1,4 @@
+try:
+    5/0
+except ZeroDivisionError:
+    print("Dividing a number by ZERO does not make sense.")
