df843fba73b9f9e42739f2e0a6ea19c2823ffdab
Tianyu W <tianyuelite@gmail.com>
2022-09-07 12:32:26 -0400

    add unit 07

---
+++ Unit 07/exercise_find_vowels.py
@@ -0,0 +1,10 @@
+def findVowels(s):
+    vowels = 'aeiouAEIOUyY'
+    count = 0
+    for c in s:
+        if c in vowels:
+            count += 1
+
+    return count
+
+print(findVowels('Halloween'))
