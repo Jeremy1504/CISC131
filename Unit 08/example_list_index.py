e98fa2184b79040d9b79630b02bb0d966a7999a0
Tianyu W <tianyuelite@gmail.com>
2022-09-08 21:11:04 -0400

    add more examples

--- Unit 08/example_list_index.py
+++ Unit 08/example_list_index.py
@@ -4,6 +4,7 @@ print(myList[0])
 print(myList[2])
 print(myList[7])
 
+# use try except to catch IndexError
 try:
     print(myList[12])
     print(myList[15])
@@ -11,6 +12,7 @@ except IndexError:
     print('Index out of range.')
 print(myList[-1])
 
+# use try except to catch IndexError
 try:
     print(myList[-13])
 except IndexError:
