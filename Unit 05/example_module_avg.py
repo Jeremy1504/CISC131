72bb65196b80e5ab2750edbcfe4919b2452a2978
Tianyu W <tianyuelite@gmail.com>
2022-10-11 11:31:58 -0400

    update

--- Unit 05/example_module_avg.py
+++ Unit 05/example_module_avg.py
@@ -1,4 +1,13 @@
-import random
-
+# module examples
+# a function to caculate the average of two numbers
 def avgTwo(a, b):
     return (a+b)/2
+
+def avgThree(a, b, c):
+    return (a+b+c)/3
+
+def sumTwo(a, b):
+    return a+b
+
+def sumThree(a, b, c):
+    return a+b+c
