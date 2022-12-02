49913c7fab26fc9c0b995fb277cf1cda69bdef01
Tianyu W <tianyuelite@gmail.com>
2022-12-02 11:42:17 -0500

    add comments

--- Project/PasswordGenerator/template.py
+++ Project/PasswordGenerator/template.py
@@ -1,26 +1,29 @@
-import random
-
 import tkinter
 from tkinter import Label, Entry, Text, StringVar, Button, END
 '''
 A simple example to wrap a password generator in a GUI app.
 '''
 def generatePassword(n):
+    '''
+    need to modify
+    use 'hello world' for place-holder
+    '''
     return 'hello world ' * n
 
 
 def main():
-
+    # create a Tk() window
     window = tkinter.Tk()
     # set title
     window.title("Password Generator")
 
     # Creating Label widgets
-    input_label = Label(window, text = "Enter the length of password")
+    input_label = Label(window, text = "Enter a number")
     # Create a StringVar()
     value = StringVar()
-
+    # input box
     input_value = Entry(window, textvariable = value)
+    # output label
     output_label = Label(window, text = 'Result')
 
     # grid alignment
@@ -28,16 +31,19 @@ def main():
     input_value.grid(row = 0, column = 1, pady=10, padx=10)
     output_label.grid(row = 4, column = 0, pady=10, padx=10)
 
-    # Creating Text Widgets
+    # Creating Text Widget as output value
     output_text = Text(window, height = 1, width = 30)
 
     def trigger_function():
+        # call function to calculate
         result = generatePassword(int(value.get()))
+        # clear content (startIndex, endIndex)
         output_text.delete("1.0", END)
+        # update results (index, String)
         output_text.insert(END, result)
 
-    # Creating  Button Widget
-    button_convert = Button(window, text = "Generate", command = trigger_function)
+    # Creating Button Widget
+    button_convert = Button(window, text = "Click", command = trigger_function)
     
     # grid alignment
     output_text.grid(row = 4, column = 1, pady=10, padx=10)
